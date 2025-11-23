f88051ae7a6dd295dc086c1d3345f5b49e5f8595
Fabrice <husunukpefabrice@gmail.com>
2025-11-23 19:08:39 +0000

    Rediriger la page d'accueil vers la page de connexion

--- attendance_system/urls.py
+++ attendance_system/urls.py
@@ -15,14 +15,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     # Administration Django
     path('admin/', admin.site.urls),
     
-    # Landing page
-    path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
+    # Redirection vers la page de connexion
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=False), name='home'),
     
     # Interface utilisateur
     path('accounts/', include('accounts.urls')),
